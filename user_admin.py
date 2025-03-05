689309393d7005aac3bca9c47dcbb9b4bc4502c5
Labhesh <labheshk356@gmail.com>
2025-03-05 11:17:44 +0530

    Here i have done majority for admin roles

--- user_admin.py
+++ user_admin.py
@@ -1,24 +1,32 @@
-import sqlite3
 from database import adder
 Adder=adder()
-
-data=sqlite3.connect("mydatabase.db")
-cursor=data.cursor()
 class Admin:
     def __init__(self):
         pass
     def addmovie(self):
         movie_amount=int(input("How many movies you want to add:"))
         for i in range(movie_amount):
-            movie=input(f"Enter Movie No.{i+1}")
-            movie_genre=input("Genre of this movie? : ")
-            ticket_set=int(input("Enter no of tickets to Alot this show: "))
-
+            values=input(f"Enter Movie and info of {i+1}: ")
+            info=values.split(",")
+            movie=info[0]
+            movie_genre=info[1]
+            ticket_set=int(info[2])
             Adder.add(name=movie,genre=movie_genre,seats=ticket_set)
 
     def removemovie(self):
         trash=input("Which Movie you want to remove: ")
         Adder.remove(name=trash)
+
+    def updateinfo(self):
+        Adder.printer()
+        movie=input("Which movie's info you want to change: ")
+        domain=input("Which Domain you want to Change (name, genre, tickets_available): ")
+        updated_value=input("Updated Value: ")
+        Adder.update(domain=domain,movie=movie,updated=updated_value)
+
+
+
+
 
 
 
