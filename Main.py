3314a74b28c59a568f46905af14b5a0b1cb1dbd1
Labhesh <labheshk356@gmail.com>
2025-03-06 08:59:27 +0530

    updated

--- Main.py
+++ Main.py
@@ -1,27 +1,10 @@
-import flask
-import os
+from Authentication import UserInfo
 from Movies import Movie,Booking
+entry=UserInfo()
+movie=Movie()
+book=Booking()
 from user_admin import Admin
 import user_admin
-
 admin=Admin()
-movie=Movie()
-booking=Booking()
-Username=input("Please Enter Your Username: ")
-Password=input("Please Enter You Password: ")
-if Username=="Admin" and Password=="Admin@123":
-    os.system("cls")
-    print("Welcome Admin! Which Movie Would you Add or Remove Today!?")
-    des=input()
-    if des=="Add":
-        admin.addmovie()
-        print("Updated list is:")
-        print(user_admin.list1)
-    elif des=="Remove":
-        admin.removemovie()
-        print("Updated List is:")
-        print(user_admin.list1)
-else:
-    print("Which Movie Would you like to watch Today!")
-    movie.screening()
-    booking.booking()
+user=UserInfo()
+user.Authenticator()
